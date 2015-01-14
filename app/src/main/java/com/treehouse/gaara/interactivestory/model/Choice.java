4f6c581069854c7313cd85253ba225e8d1ec3ce0
Juan Gomez <erioly@gmail.com>
2015-01-13 23:42:47 -0500

    Initial commit

---
+++ app/src/main/java/com/treehouse/gaara/interactivestory/model/Choice.java
@@ -0,0 +1,11 @@
+package com.treehouse.gaara.interactivestory.model;
+
+/**
+ * Created by Gaara on 1/2/2015.
+ */
+public class Choice {
+
+    private String text;
+    private int mNextPage;
+
+}
---
+++ app/src/main/java/com/treehouse/gaara/interactivestory/ui/MainActivity.java
@@ -0,0 +1,40 @@
+package com.treehouse.gaara.interactivestory.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.treehouse.gaara.interactivestory.R;
+
+
+public class MainActivity extends Activity {
+
+    private EditText mNameField;
+    private Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameField = (EditText) findViewById(R.id.nameEditText);
+        mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = mNameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
+    }
+}
